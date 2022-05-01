d8428b5dd4f61827c9c3dd99be975eb34d6e6b2a
Animesh Murmu <am2646374@gmail.com>
2022-05-01 17:25:41 +0530

    constructor args

--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -2,13 +2,19 @@ package example;
 
 public class Test {
     private String what = "default";
-
+    private String test = "constructor";
+    
+    public Test(String what, String test) {
+        this.what = what;
+        this.test = test;
+    }
     void say() {
-        System.out.println(what);
+        System.out.println(String.format("what %s, test %s", what, test));
     }
     public String getWhat() {
         return what;
     }
+    
     public void setWhat(String what) {
         this.what = what;
     }
