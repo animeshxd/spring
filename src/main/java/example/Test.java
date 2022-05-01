cfb104b17c5e15308e380ccde0c651a291e614a4
Animesh Murmu <am2646374@gmail.com>
2022-05-01 17:36:09 +0530

    call constructor by args type

--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -3,13 +3,17 @@ package example;
 public class Test {
     private String what = "default";
     private String test = "constructor";
+    private int id = 0;
     
+    public Test(int id) {
+        this.id = id;
+    }
     public Test(String what, String test) {
         this.what = what;
         this.test = test;
     }
     void say() {
-        System.out.println(String.format("what %s, test %s", what, test));
+        System.out.println(String.format("what %s, test %s, id=%d ", what, test, id));
     }
     public String getWhat() {
         return what;
