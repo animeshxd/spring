c57af8422aeb791a7ca07c71da5415dd9c79bd0d
Animesh Murmu <am2646374@gmail.com>
2022-05-03 20:14:20 +0530

    collections

--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -1,12 +1,21 @@
 package example;
 
+import java.util.List;
+
 public class Test {
     private String what = "default";
     private String test = "constructor";
     private int id = 0;
+    private List<Integer> list;
     
     public Test(String what) {
         this.what = what;
+    }
+    public List<Integer> getList() {
+        return list;
+    }
+    public void setList(List<Integer> list) {
+        this.list = list;
     }
     public Test(int id) {
         this.id = id;
@@ -17,6 +26,9 @@ public class Test {
     }
     void say() {
         System.out.println(String.format("what=%s, test=%s, id=%d ", what, test, id));
+        for (int l: list) {
+            System.out.println(l);
+        }
     }
     public String getWhat() {
         return what;
