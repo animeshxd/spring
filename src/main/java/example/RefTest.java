89b40876f198fcfce9e6266bb7900d53d7bfd0d3
Animesh Murmu <am2646374@gmail.com>
2022-05-06 10:26:47 +0530

    autowire byName

--- src/main/java/example/RefTest.java
+++ src/main/java/example/RefTest.java
@@ -6,8 +6,19 @@ public class RefTest {
     public RefTest(Info info) {
         this.info = info;
     }
+    /**
+     * Fix for "No default constructor found"
+     *  (We need to explicitly define no args constructor for autowire="byName")
+     * */
+    public RefTest(){} 
     public void print(){
         System.out.printf("id=%d, name=%s \n", info.getId(), info.getName());
+    }
+    public Info getInfo() {
+        return info;
+    }
+    public void setInfo(Info info) {
+        this.info = info;
     }
 
 }
