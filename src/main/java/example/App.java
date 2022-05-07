a89ba2c507ef59079493ebcb5e2867976347214e
Animesh Murmu <am2646374@gmail.com>
2022-05-07 20:27:40 +0530

    cleanup

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -8,16 +8,7 @@ public class App
     public static void main( String[] args )
     {
         ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
-        // Test test = (Test) ctx.getBean("test");
-        // test.say();
-
-        // RefTest tst = (RefTest) ctx.getBean("reftest");
-        // tst.print();
-        // RefTest tst1 = (RefTest) ctx.getBean("reftest");
-        // tst1.print();
-
-        Aware aware = (Aware) ctx.getBean("aware");
-        aware.print();
-        aware.printInfo();
+        
+        
     }
 }
