d885f27987b8d636b6e1fb0aef0c979b72b55cf9
Animesh Murmu <am2646374@gmail.com>
2022-05-09 18:40:03 +0530

    Lifecycle Callbacks - calling afterPropertiesSet method from spring.xml file

--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -2,10 +2,7 @@ package example;
 
 import java.util.List;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class Test implements InitializingBean, DisposableBean{
+public class Test {
     List<Integer> list;
     
     public Test(List<Integer> list) {
@@ -19,13 +16,13 @@ public class Test implements InitializingBean, DisposableBean{
         }
     }
 
-    @Override
+    
     public void afterPropertiesSet() throws Exception {
        System.out.println("afterPropertiesSet() called for Test");
         
     }
 
-    @Override
+    
     public void destroy() throws Exception {
         
         System.out.println("destroy() method called for Test");
