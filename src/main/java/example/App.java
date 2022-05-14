9c2c88bf65794e0acbf6908f780840a8b7f278d1
Animesh Murmu <am2646374@gmail.com>
2022-05-14 17:07:38 +0530

    cleanup

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -9,13 +9,6 @@ public class App
     {
         AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
         ctx.registerShutdownHook();
-        // Info info1 = (Info) ctx.getBean("info1");
-        // info1.print();
 
-        // Info info2 = (Info) ctx.getBean("info2");
-        // info2.print();
-
-        Test test = (Test) ctx.getBean("test");
-        test.print();
     }
 }
