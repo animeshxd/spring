c363f9314bc58f41f2d0782a75eef53d97f92313
Animesh Murmu <am2646374@gmail.com>
2022-06-24 10:19:41 +0530

    Injection using Value Annotation

--- src/main/java/example/Test.java
+++ src/main/java/example/Test.java
@@ -4,47 +4,43 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.annotation.Resource;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Test {
-    List<String> list;
-    Map<Integer, String> map;
-    Set<String> set;
     
-    /**
-     * @Aurowire
-     * @Qualifier("list")
-     * public void setList(List<String> list) {
-     *   this.list = list;
-     * }
-     * This annotation will not work, as It will try to autowire
-     * list of String beans which is not Defined
-     * 
-     * 
-     * 
-     */
-    @Resource()
-    public void setList(List<String> list) {
-        this.list = list;
-    }
-    
-    @Resource()
-    public void setMap(Map<Integer, String> map) {
-        this.map = map;
-    }
+    // https://docs.spring.io/spring-framework/docs/current/reference/html/core.html#expressions
+    @Value("StringValue")
+    public String value;
 
-    @Resource()
-    public void setSet(Set<String> set) {
-        this.set = set;
-    }
+    @Value("#{list}")
+    public List<String> list;
+
+    @Value("#{map}")
+    public Map<Integer, String> map;
+
+    @Value("#{map['2']}") // @Value("#{map[2]}") will not work
+    public String value2;
+
+    @Value("#{set}")
+    public Set<String> set;
+
+    @Value("#{ systemProperties['os.name'] }")
+    public String os;
+
+    //@Value("#{ systemEnvironment['NUMBER_OF_PROCESSORS'] }")
+    @Value("${NUMBER_OF_PROCESSORS}")
+    public String processor;
+
+    @Value("${UNKNOWN:Default Value}")
+    public String unknownenv;
 
 
     @Override
     public String toString() {
-        return "Test [list=" + list + ", map=" + map + ", set=" + set + "]";
+        return "Test [list=" + list + ", map=" + map + ", os=" + os + ", processor=" + processor + ", set=" + set
+                + ", unknownenv=" + unknownenv + ", value=" + value + ", value2=" + value2 + "]";
     }
     
 }
