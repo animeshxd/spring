c020edc7990d68cf8ee9795d96d5a5cc670352db
Animesh Murmu <am2646374@gmail.com>
2022-06-25 10:26:06 +0530

    Java-based Container Configuration - setup

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -1,15 +1,10 @@
 package example;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Test test = (Test) context.getBean("test");
-        test.print();
-        
-        context.getBean("test");
-        context.getBean("test");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
     }
 }
